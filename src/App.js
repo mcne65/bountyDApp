038526afdf072adba1e0e0c66256dade5b5ae9e0
s4ndhyac <s4ndhyac@gmail.com>
2018-08-15 12:30:27 +0530

    using bootstrap

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './css/oswald.css'
 import './css/open-sans.css'
 import './css/pure-min.css'
 import './App.css'
+import {Jumbotron} from 'reactstrap'
 
 class App extends Component {
   constructor(props) {
@@ -18,8 +19,6 @@ class App extends Component {
   }
 
   componentWillMount() {
-    // Get network provider and web3 instance.
-    // See utils/getWeb3 for more info.
 
     getWeb3
       .then(results => {
@@ -27,7 +26,6 @@ class App extends Component {
           web3: results.web3
         })
 
-        // Instantiate contract once web3 provided.
         this.instantiateContract()
       })
       .catch(() => {
@@ -36,18 +34,12 @@ class App extends Component {
   }
 
   instantiateContract() {
-    /*
-     * SMART CONTRACT EXAMPLE
-     *
-     * Normally these functions would be called in the context of a
-     * state management library, but for convenience I've placed them here.
-     */
 
     const contract = require('truffle-contract')
     const bountyContract = contract(BountyContract)
     bountyContract.setProvider(this.state.web3.currentProvider)
 
-    // Declaring this for later so we can chain functions on SimpleStorage.
+    // Declaring this for later so we can chain functions on bountyContract.
     var bountyContractInstance
 
     // Get accounts.
@@ -56,7 +48,7 @@ class App extends Component {
         bountyContractInstance = instance
 
         // Stores a given value, 5 by default.
-        return bountyContractInstance.set(5, { from: accounts[0] })
+        return bountyContractInstance.set(10, { from: accounts[0] })
       }).then((result) => {
         // Get the value from the contract to prove it worked.
         return bountyContractInstance.get.call(accounts[0])
@@ -70,22 +62,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <nav className="navbar pure-menu pure-menu-horizontal">
-          <a href="#" className="pure-menu-heading pure-menu-link">Truffle Box</a>
-        </nav>
+      <Jumbotron>
+      <h1>BountyDApp: A Decentralized Application for Bounty Creators and Hunters</h1>
+      </Jumbotron>
 
-        <main className="container">
-          <div className="pure-g">
-            <div className="pure-u-1-1">
-              <h1>Good to Go!</h1>
-              <p>Your Truffle Box is installed and ready.</p>
-              <h2>Smart Contract Example</h2>
-              <p>If your contracts compiled and migrated successfully, below will show a stored value of 5 (by default).</p>
-              <p>Try changing the value stored on <strong>line 59</strong> of App.js.</p>
-              <p>The stored value is: {this.state.storageValue}</p>
-            </div>
-          </div>
-        </main>
       </div>
     );
   }

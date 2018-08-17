e766ea989e380003ec4f17bda9a635dbaf7ba6ce
s4ndhyac <s4ndhyac@gmail.com>
2018-08-18 01:45:02 +0530

    temp commit dashboard -> My contracts outline

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -7,7 +7,10 @@ import './css/oswald.css'
 import './css/open-sans.css'
 import './css/pure-min.css'
 import './App.css'
-import { Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink, Nav, Collapse, Row, Col } from 'reactstrap'
+import {
+    Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink, Nav, Collapse, Card, Button, CardHeader, CardFooter, CardBody,
+    CardTitle, CardText, Col, Label, Input, FormGroup, Row
+} from 'reactstrap'
 import { Link } from 'react-router-dom'
 
 
@@ -16,7 +19,8 @@ class Dashboard extends Component {
         super(props)
         this.state = {
             web3: null,
-            account: null
+            account: null,
+            bounties: []
         }
         this.bountyContract = contract(BountyContract)
     }
@@ -33,6 +37,8 @@ class Dashboard extends Component {
             })
             .catch(() => {
                 console.log('Error finding web3.')
+            }).then(() => {
+                this.getMyBounties()
             })
     }
 
@@ -47,6 +53,57 @@ class Dashboard extends Component {
                 this.setState({ account: accounts[0] });
             })
         })
+    }
+
+    getMyBounties() {
+        var bountyContractInstance;
+        this.bountyContract.deployed().then((instance) => {
+            bountyContractInstance = instance;
+            var bountiesCreated = []
+            bountyContractInstance.returnBountiesCount().then((numBounties) => {
+                for (var i = 0; i < numBounties; i++) {
+                    bountyContractInstance.getBounty.call(i, { from: this.state.account }).then((bounty) => {
+                        if (bounty[0] == this.state.account) {
+                            bountiesCreated.push(bounty)
+                            this.setState({ bounties: bountiesCreated })
+                        }
+                    })
+                }
+            })
+        })
+    }
+
+    getSolutions() {
+
+
+    }
+
+    createMyBountiesCard(bounty, index) {
+        var bountyStage = bounty[3].valueOf() == 0 ? "Open" : bounty[3].valueOf() == 1 ? "Closed" : "Invalid State"
+        return (
+            <Card key={"bountyId_" + index}>
+                <CardHeader tag="h3">{bountyStage}</CardHeader>
+                <CardBody>
+                    <CardTitle>Problem Statement</CardTitle>
+                    <CardText className="lead">{this.state.web3.toAscii(bounty[1])}</CardText>
+                    {/* <hr />
+                    {bounty[3].valueOf() == 0 ? (
+                        <div>
+                            <FormGroup row>
+                                <Col sm={1}>
+                                    <Label for="bountySolution" size="lg">Solution</Label>
+                                </Col>
+                                <Col sm={8}>
+                                    <Input type="solution" ref="solutionRef" name="solution" id="solution" placeholder="Enter Solution" bsSize="lg" />
+                                </Col>
+                            </FormGroup>
+                            <Button onClick={() => this.submitSolution(index)}>Submit Solution</Button>
+                        </div>
+                    ) : null} */}
+                </CardBody>
+                <CardFooter tag="h3">{"Reward: " + bounty[2].valueOf() + " ETH"}</CardFooter>
+            </Card>
+        )
     }
 
 
@@ -78,12 +135,15 @@ class Dashboard extends Component {
                     <Row>
                         <Col className="col-sm-3 col-md-2 sidebar">
                             <Nav vertical>
-                                <NavItem><NavLink id="navbar-vertical" href="#" active>My Contracts</NavLink></NavItem>
+                                <NavItem><NavLink id="navbar-vertical" href="#" active>My Bounties</NavLink></NavItem>
                                 <NavItem><NavLink id="navbar-vertical" href="#">My Submissions</NavLink></NavItem>
                                 <NavItem><NavLink id="navbar-vertical" href="#">My Rewards</NavLink></NavItem>
                             </Nav>
                         </Col>
                         <Col className="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+                            <div id="myBounties">
+                                {this.state.bounties.map((bounty, index) => { return this.createMyBountiesCard(bounty, index) })}
+                            </div>
 
                         </Col>
                     </Row>

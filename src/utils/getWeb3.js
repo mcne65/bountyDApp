79c173850c6bd12708ded8c4426f0f7c1b5322c9
s4ndhyac <s4ndhyac@gmail.com>
2018-08-20 14:17:36 +0530

    setting fallback web3 provider to localhost:8545

--- src/utils/getWeb3.js
+++ src/utils/getWeb3.js
@@ -1,8 +1,8 @@
 import Web3 from 'web3'
 
-let getWeb3 = new Promise(function(resolve, reject) {
+let getWeb3 = new Promise(function (resolve, reject) {
   // Wait for loading completion to avoid race conditions with web3 injection timing.
-  window.addEventListener('load', function() {
+  window.addEventListener('load', function () {
     var results
     var web3 = window.web3
 
@@ -21,7 +21,7 @@ let getWeb3 = new Promise(function(resolve, reject) {
     } else {
       // Fallback to localhost if no web3 injection. We've configured this to
       // use the development console's port by default.
-      var provider = new Web3.providers.HttpProvider('http://127.0.0.1:9545')
+      var provider = new Web3.providers.HttpProvider('http://127.0.0.1:8545')
 
       web3 = new Web3(provider)
 
